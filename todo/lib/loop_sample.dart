e21e4dc0a6dec046b95aa548e1e17e6b1aebf4ab
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-10-25 22:03:57 +0900

    feat: create loop_sample.dart

---
+++ todo/lib/loop_sample.dart
@@ -0,0 +1,37 @@
+void loopSample() {
+  for (int i = 0; i < 3; ++i) {
+    print("index = $i");
+  }
+
+  final list = [1, 2, 3];
+  for (final element in list) {
+    print("element = $element");
+  }
+  list.forEach((element) {
+    print("element = $element");
+  });
+
+  var flag = true;
+  var count = 0;
+  while (flag) {
+    if (count == 10) flag = false;
+    print("while execute");
+    count++;
+  }
+
+  do {
+    print("do-while execute");
+  } while (flag);
+
+  for (int i = 0; i < 10; ++i) {
+    if (i % 2 == 0) {
+      continue;
+    }
+
+    if (i > 6) {
+      break;
+    }
+
+    print("index = $i");
+  }
+}
