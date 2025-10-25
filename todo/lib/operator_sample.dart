8ade335f2d314decc809a8c838c4dc471ae9de95
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-10-25 13:31:46 +0900

    feat: add operator sample

---
+++ todo/lib/operator_sample.dart
@@ -0,0 +1,21 @@
+void operatorSample1() {
+  print(1 + 2);
+  print(1 - 2);
+  print(1 * 2);
+  print(1 / 2);
+  print(1 % 2);
+  print(1 ~/ 2);
+  print(1 == 2);
+  print(1 != 2);
+  print(1 > 2);
+  print(1 < 2);
+  print(1 >= 2);
+  print(1 <= 2);
+}
+
+void conditionalOperatorSample() {
+  int a = 123;
+  int b = 456;
+  final max = a > b ? a : b;
+  print(max);
+}
