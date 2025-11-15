51c8faa429148e514da037a6bfa4cf73dedf4844
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-11-15 13:41:53 +0900

    feat: create abstract class and implements keyword demo

---
+++ todo/lib/abstract_class_and_implements_sample.dart
@@ -0,0 +1,18 @@
+abstract class Animal {
+  String get name;
+  set name(String name);
+
+  void sayHello();
+}
+
+class Duck implements Animal {
+  Duck(this.name);
+
+  @override
+  String name;
+
+  @override
+  void sayHello() {
+    print("Duck Duck Duck");
+  }
+}
