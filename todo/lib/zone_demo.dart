04568fe468662da32ae10f36aa407141ff6558ac
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-11-22 13:19:32 +0900

    feat: create zone demo

---
+++ todo/lib/zone_demo.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+Future<String> fetchUsername() {
+  var str = Future.delayed(const Duration(seconds: 1), () => throw "User not found.");
+  return str;
+}
+
+void zoneDemo() {
+  fetchUsername().then((data) {
+    print(data);
+  });
+
+  runZonedGuarded(
+    () {
+      fetchUsername().then((data) {
+        print(data);
+      });
+    },
+    (error, stackTrace) {
+      print("Caught: $error");
+    },
+  );
+}
