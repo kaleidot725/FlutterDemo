3d7e3d453b91d587c92d1e3a321279e3bf77b5ba
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-12-28 21:35:19 +0900

    feat: update

--- theme_demo/lib/main.dart
+++ theme_demo/lib/main.dart
@@ -4,119 +4,87 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// ThemeExtensionを継承して、独自のテーマ属性を定義・管理するクラス
+class MyTheme extends ThemeExtension<MyTheme> {
+  const MyTheme({required this.themeColor});
 
-  // This widget is the root of your application.
+  // ① このテーマでは独自のthemeColorを定義する
+  final Color? themeColor;
+
+  // ② もし①のthemeColorが存在すれば、①のthemeColorを返す
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  MyTheme copyWith({Color? themeColor}) {
+    return MyTheme(themeColor: themeColor ?? this.themeColor);
+  }
+
+  // ③ テーマの変化を線形的にするためのメソッドらしい
+  @override
+  MyTheme lerp(MyTheme other, double t) {
+    return MyTheme(themeColor: Color.lerp(themeColor, other.themeColor, t));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  // ダークモードかどうかの状態を保持するフラグ
+  bool _isDarkMode = false;
 
-  void _incrementCounter() {
+  // ダークモードのON/OFFを切り替えるメソッド
+  void _toggleDarkMode() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isDarkMode = !_isDarkMode;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      // ライトモード時のテーマ設定
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        // MyTheme拡張を登録し、themeColorを「青」に設定
+        extensions: const [MyTheme(themeColor: Color(0xFF0000FF))],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      // ダークモード時のテーマ設定
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        brightness: Brightness.dark,
+        // MyTheme拡張を登録し、themeColorを「赤」に設定
+        extensions: const [MyTheme(themeColor: Color(0xFFFF0000))],
+      ),
+      // _isDarkModeフラグに応じて、明示的にテーマモード（Light/Dark）を切り替える
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        body: const Center(child: ThemedWidget()),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _toggleDarkMode();
+          },
+          child: const Icon(Icons.settings_brightness),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+/// カスタムテーマ(MyTheme)を利用して描画を行うウィジェット
+class ThemedWidget extends StatelessWidget {
+  const ThemedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 現在のBuildContextに関連付けられたテーマから、MyTheme拡張を取得する
+    final myTheme = Theme.of(context).extension<MyTheme>()!;
+    // テーマに設定されている色(themeColor)を取り出す
+    final color = myTheme.themeColor;
+    return Container(width: 100, height: 100, color: color);
   }
 }
